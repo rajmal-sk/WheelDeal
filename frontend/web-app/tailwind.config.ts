91b8c9ea4f20822b56043d80d58d2a6c446ce5a6
rajmal-sk <146296263+rajmal-sk@users.noreply.github.com>
2024-07-13 18:28:01 -0500

    created frontend web-app

---
+++ frontend/web-app/tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+const flowbite = require("flowbite-react/tailwind");
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  // To be commented
+  corePlugins: {
+    aspectRatio: false
+  },
+  // To be commented
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    flowbite.plugin(),
+  ],
+};
+export default config;
